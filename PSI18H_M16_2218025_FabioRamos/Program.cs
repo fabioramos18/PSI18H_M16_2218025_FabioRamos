768ea1cebbfd303b2fb98b4f64d9f4fc44f2dd63
fabio <fabio@DESKTOP-3TB5OG5>
2020-05-12 18:47:11 +0100

    12 de maio

---
+++ PSI18H_M16_2218025_FabioRamos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSI18H_M16_2218025_FabioRamos
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
